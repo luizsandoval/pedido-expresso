0f35ae44d5ccd0850cf59696affb27b38cef07a2
Luiz Sandoval <luiz.sandoval@global.ntt>
2023-09-24 18:07:34 -0300

    feat(clients): adds title for clients form page

--- src/app/clients/form/page.tsx
+++ src/app/clients/form/page.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from 'next';
+
 import { Form } from '@/components/pages/clients/Form';
 import { Card } from '@/components/shared/Card';
 import { Title } from '@/components/shared/Title';
 import { PageProps } from '@/models/page-props';
+
+export const metadata: Metadata = {
+    title: 'Formulário de clientes',
+};
 
 const ClientForm = ({ searchParams }: PageProps) => {
     const isEdit = !!searchParams['_id'];
