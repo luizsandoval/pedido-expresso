f607e7464bd7d8f69e47f6813b764440e2a2ca71
Luiz Sandoval <luiz.sandoval@global.ntt>
2023-09-25 20:46:55 -0300

    refactor: removes exit button from header

--- src/components/pages/home/Header.tsx
+++ src/components/pages/home/Header.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export const Header = () => (
-  <header className="mx-auto flex h-16 items-center justify-between px-6 py-4 border-b-2 border-b-gray-100 bg-white w-full">
-    <Image src="/logo.svg" width={40} height={40} alt="truck" />
-    <button
-        className='text-red-300 bg-transparent border-none'
-    >
-        SAIR
-    </button>
-  </header>
+    <header className="mx-auto flex h-16 w-full items-center justify-center border-b-2 border-b-gray-100 bg-white px-6 py-4">
+        <Image src="/logo.svg" width={40} height={40} alt="truck" />
+    </header>
 );
