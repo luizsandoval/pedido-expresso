079314754f3969108f4d669ecb22300b22783fe4
Luiz Sandoval <luiz.sandoval@global.ntt>
2023-10-02 11:35:37 -0300

    refactor: changes project name to 'pedido-expresso' and updates logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: {
-        template: '%s | POC Pinheiro',
+        template: '%s | Pedido expresso',
         absolute: '',
     },
 };
