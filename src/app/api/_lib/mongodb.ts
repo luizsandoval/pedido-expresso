828343cb1f5608c050b93236f32d66226a3c7ad7
Luiz Sandoval <luiz.sandoval@global.ntt>
2023-10-06 09:54:47 -0300

    fix(mongodb): removes throw from mongodb handler

--- src/app/api/_lib/mongodb.ts
+++ src/app/api/_lib/mongodb.ts
@@ -1,10 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-if (!process.env.MONGODB_URI) {
-    throw new Error('Please add your Mongo URI to .env.local');
-}
-
-const uri: string = process.env.MONGODB_URI;
+const uri: string = process.env.MONGODB_URI || '';
 
 let mongoClient: MongoClient;
 let mongoClientPromise: Promise<MongoClient>;
